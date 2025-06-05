2bb9eadd0ed0f0a63b4fd19b8b6472bfc2b7331f
Parth-successiveTech <Parth.yadav@successive.tech>
2025-06-05 16:34:21 +0530

    changes aa 4 30

---
+++ Assignment-4/Question2.js
@@ -0,0 +1,25 @@
+" use strict"
+const ps=require("prompt-sync");
+const prompt=ps();
+let  sentence=prompt( "enter the input");
+let arr=[];
+let ans="";
+for(let i=0;i<sentence.length;i++)
+{
+    arr.push(sentence[i]);
+    if(sentence[i]==" ")
+    {
+        while(arr.length>0)
+        {
+            ans=ans+arr.pop();
+        }
+    }
+    ans=ans+" ";
+    
+}
+ans=ans+" ";
+while(arr.length>0)
+{
+    ans=ans+arr.pop();
+}
+console.log(ans);
