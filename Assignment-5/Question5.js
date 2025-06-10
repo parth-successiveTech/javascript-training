8161d0965ef87f8931dca4fd37482c908f589e63
Parth-successiveTech <Parth.yadav@successive.tech>
2025-06-10 17:59:31 +0530

    5th question has been completed

--- Assignment-5/Question5.js
+++ Assignment-5/Question5.js
@@ -1,36 +1,20 @@
 const obj = {
     a: 1,
     b: { c: 2, d: [3, 4] }
-  };
-  function deepCopy(obj)
-  {
-    if(obj===null|| typeof obj!=='object')
-    {
-        return obj;
-    }
-        if(Array.isArray(obj))
-        {
-            const copyArray=[];
-            for(let idx in obj)
-            {
-                
-                copyArray[idx]=deepCopy(obj[idx]);
-            }
-            
-            return copyArray;
+  }
+  function flatten(obj) {
+    const result = {};
+    for (const key of Object.keys(obj)) {
+      if (typeof obj[key] === 'object') {
+        const nested = flatten(obj[key]);
+        for (const nestedKey of Object.keys(nested)) {
+          result[`${key}.${nestedKey}`] = nested[nestedKey];
         }
-      const objCopy={};
-
-        for(const  key in obj)
-        {
-            objCopy[key]=deepCopy(obj[key]);
-
-        }
-        return objCopy;
-
+      } else {
+        result[key] = obj[key];
+      }
     }
-
-  let obj2=deepCopy(obj);
-  obj2.b.d[0] = 100;
-  console.log(obj);
-  console.log(obj2);
+    return result;
+  }
+ ans=flatten(obj);
+console.log(ans);
---
+++ Assignment-6/Question2.js
@@ -0,0 +1,20 @@
+class shape{
+    constructor(height,width)
+    {
+        this.height=height;
+        this.width=width;
+
+        getArea()
+        {
+
+
+        }
+    }
+}
+class Rectangle extends shape
+{
+     obj=new 
+
+    
+    
+}
